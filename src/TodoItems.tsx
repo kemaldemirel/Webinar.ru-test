b622d42c7cb3bd54534e980840082cfa51d00edf
Кемал Аджимамбетов <demirelkd0696@gmail.com>
2021-11-04 15:42:46 +0300

    Добавил возможность редактирования

--- src/TodoItems.tsx
+++ src/TodoItems.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import { useCallback } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import DoneIcon from '@material-ui/icons/Done';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -13,109 +18,168 @@ import { motion } from 'framer-motion';
 import { TodoItem, useTodoItems } from './TodoItemsContext';
 
 const spring = {
-    type: 'spring',
-    damping: 25,
-    stiffness: 120,
-    duration: 0.25,
+  type: 'spring',
+  damping: 25,
+  stiffness: 120,
+  duration: 0.25,
 };
 
 const useTodoItemListStyles = makeStyles({
-    root: {
-        listStyle: 'none',
-        padding: 0,
-    },
+  root: {
+    listStyle: 'none',
+    padding: 0,
+  },
 });
 
 export const TodoItemsList = function () {
-    const { todoItems } = useTodoItems();
+  const { todoItems } = useTodoItems();
 
-    const classes = useTodoItemListStyles();
+  const classes = useTodoItemListStyles();
 
-    const sortedItems = todoItems.slice().sort((a, b) => {
-        if (a.done && !b.done) {
-            return 1;
-        }
+  const sortedItems = todoItems.slice().sort((a, b) => {
+    if (a.done && !b.done) {
+      return 1;
+    }
 
-        if (!a.done && b.done) {
-            return -1;
-        }
+    if (!a.done && b.done) {
+      return -1;
+    }
 
-        return 0;
-    });
+    return 0;
+  });
 
-    return (
-        <ul className={classes.root}>
-            {sortedItems.map((item) => (
-                <motion.li key={item.id} transition={spring} layout={true}>
-                    <TodoItemCard item={item} />
-                </motion.li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={classes.root}>
+      {sortedItems.map((item) => (
+        <motion.li key={item.id} transition={spring} layout={true}>
+          <TodoItemCard item={item} />
+        </motion.li>
+      ))}
+    </ul>
+  );
 };
 
 const useTodoItemCardStyles = makeStyles({
-    root: {
-        marginTop: 24,
-        marginBottom: 24,
-    },
-    doneRoot: {
-        textDecoration: 'line-through',
-        color: '#888888',
-    },
+  root: {
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  doneRoot: {
+    textDecoration: 'line-through',
+    color: '#888888',
+  },
 });
 
 export const TodoItemCard = function ({ item }: { item: TodoItem }) {
-    const classes = useTodoItemCardStyles();
-    const { dispatch } = useTodoItems();
+  const [isEditable, setEditable] = React.useState(false);
+  const { control, handleSubmit, reset } = useForm();
 
-    const handleDelete = useCallback(
-        () => dispatch({ type: 'delete', data: { id: item.id } }),
-        [item.id, dispatch],
-    );
+  const classes = useTodoItemCardStyles();
+  const { dispatch } = useTodoItems();
 
-    const handleToggleDone = useCallback(
-        () =>
-            dispatch({
-                type: 'toggleDone',
-                data: { id: item.id },
-            }),
-        [item.id, dispatch],
-    );
+  const handleDelete = useCallback(
+    () => dispatch({ type: 'delete', data: { id: item.id } }),
+    [item.id, dispatch],
+  );
 
-    return (
-        <Card
-            className={classnames(classes.root, {
-                [classes.doneRoot]: item.done,
-            })}
-        >
-            <CardHeader
-                action={
-                    <IconButton aria-label="delete" onClick={handleDelete}>
-                        <DeleteIcon />
-                    </IconButton>
-                }
-                title={
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={item.done}
-                                onChange={handleToggleDone}
-                                name={`checked-${item.id}`}
-                                color="primary"
-                            />
-                        }
-                        label={item.title}
-                    />
-                }
+  const handleToggleDone = useCallback(
+    () =>
+      dispatch({
+        type: 'toggleDone',
+        data: { id: item.id },
+      }),
+    [item.id, dispatch],
+  );
+
+  return (
+    <Card
+      className={classnames(classes.root, {
+        [classes.doneRoot]: item.done,
+      })}>
+      <CardHeader
+        action={
+          <>
+            {isEditable ? (
+              <IconButton
+                aria-label="change"
+                onClick={handleSubmit((formData) => {
+                  dispatch({ type: 'edit', data: { id: item.id, todoItem: formData } });
+                  setEditable(false);
+                })}>
+                <DoneIcon />
+              </IconButton>
+            ) : (
+              <IconButton aria-label="change" onClick={() => setEditable(!isEditable)}>
+                <EditIcon />
+              </IconButton>
+            )}
+
+            <IconButton aria-label="delete" onClick={handleDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </>
+        }
+        title={
+          isEditable ? (
+            <form
+              onSubmit={handleSubmit((formData) => {
+                dispatch({ type: 'edit', data: { id: item.id, todoItem: formData } });
+                setEditable(false);
+              })}>
+              <Controller
+                name="title"
+                control={control}
+                defaultValue={item.title}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    size="small"
+                    {...field}
+                    label={'Заголовок задачи'}
+                    fullWidth={true}
+                    className={classes.root}
+                  />
+                )}
+              />
+              <Controller
+                name="details"
+                control={control}
+                defaultValue={item.details}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    size="small"
+                    {...field}
+                    label={'Детали задачи'}
+                    fullWidth={true}
+                    multiline={true}
+                    className={classes.root}
+                  />
+                )}
+              />
+            </form>
+          ) : (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={item.done}
+                  onChange={handleToggleDone}
+                  name={`checked-${item.id}`}
+                  color="primary"
+                />
+              }
+              label={item.title}
             />
-            {item.details ? (
-                <CardContent>
-                    <Typography variant="body2" component="p">
-                        {item.details}
-                    </Typography>
-                </CardContent>
-            ) : null}
-        </Card>
-    );
+          )
+        }
+      />
+      {item.details && !isEditable ? (
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {item.details}
+          </Typography>
+        </CardContent>
+      ) : null}
+    </Card>
+  );
 };
--- src/TodoItemsContext.tsx
+++ src/TodoItemsContext.tsx
@@ -1,115 +1,103 @@
-import {
-    createContext,
-    ReactNode,
-    useContext,
-    useEffect,
-    useReducer,
-} from 'react';
+import { createContext, ReactNode, useContext, useEffect, useReducer } from 'react';
 
 export interface TodoItem {
-    id: string;
-    title: string;
-    details?: string;
-    done: boolean;
+  id: string;
+  title: string;
+  details?: string;
+  done: boolean;
+  todoItem: object;
 }
 
 interface TodoItemsState {
-    todoItems: TodoItem[];
+  todoItems: TodoItem[];
 }
 
 interface TodoItemsAction {
-    type: 'loadState' | 'add' | 'delete' | 'toggleDone';
-    data: any;
+  type: 'loadState' | 'add' | 'delete' | 'toggleDone' | 'edit';
+  data: any;
 }
 
 const TodoItemsContext = createContext<
-    (TodoItemsState & { dispatch: (action: TodoItemsAction) => void }) | null
+  (TodoItemsState & { dispatch: (action: TodoItemsAction) => void }) | null
 >(null);
 
 const defaultState = { todoItems: [] };
 const localStorageKey = 'todoListState';
 
-export const TodoItemsContextProvider = ({
-    children,
-}: {
-    children?: ReactNode;
-}) => {
-    const [state, dispatch] = useReducer(todoItemsReducer, defaultState);
+export const TodoItemsContextProvider = ({ children }: { children?: ReactNode }) => {
+  const [state, dispatch] = useReducer(todoItemsReducer, defaultState);
 
-    useEffect(() => {
-        const savedState = localStorage.getItem(localStorageKey);
+  useEffect(() => {
+    const savedState = localStorage.getItem(localStorageKey);
 
-        if (savedState) {
-            try {
-                dispatch({ type: 'loadState', data: JSON.parse(savedState) });
-            } catch {}
-        }
-    }, []);
+    if (savedState) {
+      try {
+        dispatch({ type: 'loadState', data: JSON.parse(savedState) });
+      } catch {}
+    }
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem(localStorageKey, JSON.stringify(state));
-    }, [state]);
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state]);
 
-    return (
-        <TodoItemsContext.Provider value={{ ...state, dispatch }}>
-            {children}
-        </TodoItemsContext.Provider>
-    );
+  return (
+    <TodoItemsContext.Provider value={{ ...state, dispatch }}>{children}</TodoItemsContext.Provider>
+  );
 };
 
 export const useTodoItems = () => {
-    const todoItemsContext = useContext(TodoItemsContext);
+  const todoItemsContext = useContext(TodoItemsContext);
 
-    if (!todoItemsContext) {
-        throw new Error(
-            'useTodoItems hook should only be used inside TodoItemsContextProvider',
-        );
-    }
+  if (!todoItemsContext) {
+    throw new Error('useTodoItems hook should only be used inside TodoItemsContextProvider');
+  }
 
-    return todoItemsContext;
+  return todoItemsContext;
 };
 
 function todoItemsReducer(state: TodoItemsState, action: TodoItemsAction) {
-    switch (action.type) {
-        case 'loadState': {
-            return action.data;
-        }
-        case 'add':
-            return {
-                ...state,
-                todoItems: [
-                    { id: generateId(), done: false, ...action.data.todoItem },
-                    ...state.todoItems,
-                ],
-            };
-        case 'delete':
-            return {
-                ...state,
-                todoItems: state.todoItems.filter(
-                    ({ id }) => id !== action.data.id,
-                ),
-            };
-        case 'toggleDone':
-            const itemIndex = state.todoItems.findIndex(
-                ({ id }) => id === action.data.id,
-            );
-            const item = state.todoItems[itemIndex];
-
-            return {
-                ...state,
-                todoItems: [
-                    ...state.todoItems.slice(0, itemIndex),
-                    { ...item, done: !item.done },
-                    ...state.todoItems.slice(itemIndex + 1),
-                ],
-            };
-        default:
-            throw new Error();
+  switch (action.type) {
+    case 'loadState': {
+      return action.data;
     }
+    case 'add':
+      return {
+        ...state,
+        todoItems: [{ id: generateId(), done: false, ...action.data.todoItem }, ...state.todoItems],
+      };
+    case 'delete':
+      return {
+        ...state,
+        todoItems: state.todoItems.filter(({ id }) => id !== action.data.id),
+      };
+    case 'edit':
+      const { id, title, details } = action.data.todoItem;
+      const itemEdit = state.todoItems.find(({ id }) => id === action.data.id);
+      return {
+        ...state,
+        todoItems: [
+          ...state.todoItems.filter(({ id }) => id !== action.data.id),
+          { ...itemEdit, title: title, details: details },
+        ],
+      };
+    case 'toggleDone':
+      const itemIndex = state.todoItems.findIndex(({ id }) => id === action.data.id);
+      const item = state.todoItems[itemIndex];
+
+      return {
+        ...state,
+        todoItems: [
+          ...state.todoItems.slice(0, itemIndex),
+          { ...item, done: !item.done },
+          ...state.todoItems.slice(itemIndex + 1),
+        ],
+      };
+    default:
+      throw new Error();
+  }
 }
 
 function generateId() {
-    return `${Date.now().toString(36)}-${Math.floor(
-        Math.random() * 1e16,
-    ).toString(36)}`;
+  return `${Date.now().toString(36)}-${Math.floor(Math.random() * 1e16).toString(36)}`;
 }
